22813f47094cf89e2cc26acff2704762b26fd1a5
Chirag Patil <cp.chirag21@gmail.com>
2020-09-28 18:59:49 +0530

    Challenging problems done

--- functions/Function Practice Exercises/tempCodeRunnerFile.py
+++ functions/Function Practice Exercises/tempCodeRunnerFile.py
@@ -1,37 +1,7 @@
-def summer_69(arr):
-    flag = True
-    sum = 0
-    for num in arr:
-        if num==6:
-            flag = False
-        elif num==9:
-            flag = True
-            continue
-        if flag:
-            sum += num
-    return sum
+def print_big(letter):
+    patterns = {1:'  *  ',2:' * * ',3:'*   *',4:'*****',5:'**** ',6:'   * ',7:' *   ',8:'*   * ',9:'*    '}
+    alphabet = {'A':[1,2,4,3,3],'B':[5,3,5,3,5],'C':[4,9,9,9,4],'D':[5,3,3,3,5],'E':[4,9,4,9,4]}
+    for pattern in alphabet[letter.upper()]:
+        print(patterns[pattern])
 
-def summer69(arr):
-    flag= True
-    sum = 0
-    for num in arr:
-        while flag:
-            if num != 6:
-                sum += num
-                break
-            else:
-                flag = False
-            while not flag:
-                if num != 9:
-                    break
-                else:
-                    flag = True
-    return sum
-
-print(summer_69([1, 3, 5]))
-print(summer_69([4, 5, 6, 7, 8, 9]))
-print(summer_69([2, 1, 6, 9, 11]))
-
-print(summer69([1, 3, 5]))
-print(summer69([4, 5, 6, 7, 8, 9]))
-print(summer69([2, 1, 6, 9, 11]))
+print_big('a')
